abfcf83b53860e9e7c940c4f887b45f5267e389e
Marissa Zhong <mwz2101@columbia.edu>
2018-07-27 21:31:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,10 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist != nil
-        @songs = @artist.songs
-      else
+      if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
       end
     else
       @songs = Song.all
